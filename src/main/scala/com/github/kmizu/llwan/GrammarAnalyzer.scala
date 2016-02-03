28893a806d44b9fad179b2d3c20864181e8536ea
Kota Mizushima <mizukota@gmail.com>
2016-02-03 18:31:24 +0900

    Partially implemented

--- src/main/scala/com/github/kmizu/llwan/GrammarAnalyzer.scala
+++ src/main/scala/com/github/kmizu/llwan/GrammarAnalyzer.scala
@@ -4,15 +4,23 @@ package com.github.kmizu.llwan
   * Created by Mizushima on 2016/02/03.
   */
 object GrammarAnalyzer {
-  def calculateFirstSet(grammar: Ast.Grammar) = {
+  def calculateFirstSet(grammar: Ast.Grammar, mapping: Map[Symbol, Ast.Exp]) = {
     def first(e: Ast.Exp): Set[String] = e match {
-      case Ast.Seq(_, l, r) => ???
-      case Ast.Alt(_, l, r) => ???
-      case Ast.Ident(_, name) => ???
-      case Ast.Opt(_, e) => ???
-      case Ast.Rep0(_, e) => ???
-      case Ast.Rep1(_, e) => ???
-      case Ast.Str(_, c) => Set(c)
+      case Ast.Seq(_, l, r) =>
+        val firstOfL = first(l)
+        if(firstOfL.contains("")) first(r) else firstOfL
+      case Ast.Alt(_, l, r) =>
+        first(l) ++ first(r)
+      case Ast.Ident(_, name) =>
+        first(mapping(name))
+      case Ast.Opt(_, e) =>
+        first(e) + ""
+      case Ast.Rep0(_, e) =>
+        first(e) + ""
+      case Ast.Rep1(_, e) =>
+        first(e) + ""
+      case Ast.Str(_, c) =>
+        Set(c)
     }
     ???
   }
