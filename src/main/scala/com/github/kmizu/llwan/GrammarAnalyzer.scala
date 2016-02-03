61a58e4d21a3dda24c0a6503c177cce025a6ed66
Kota Mizushima <mizukota@gmail.com>
2016-02-03 17:27:48 +0900

    Code skelton.

---
+++ src/main/scala/com/github/kmizu/llwan/GrammarAnalyzer.scala
@@ -0,0 +1,10 @@
+package com.github.kmizu.llwan
+
+/**
+  * Created by Mizushima on 2016/02/03.
+  */
+object GrammarAnalyzer {
+  def calculateFirstSet(grammar: Ast.Grammar) = ???
+  def calculateFollowSet(grammar: Ast.Grammar) = ???
+  def calculateDirectorSet(grammar: Ast.Grammar) = ???
+}
