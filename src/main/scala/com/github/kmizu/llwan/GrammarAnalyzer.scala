388ff0b9b46b8cc69528560fb933b5fcbba0436d
Kota Mizushima <mizukota@gmail.com>
2016-02-05 20:25:43 +0900

    calculateFirstSet is defined.

--- src/main/scala/com/github/kmizu/llwan/GrammarAnalyzer.scala
+++ src/main/scala/com/github/kmizu/llwan/GrammarAnalyzer.scala
@@ -1,11 +1,12 @@
 package com.github.kmizu.llwan
 
-/**
-  * Created by Mizushima on 2016/02/03.
-  */
-object GrammarAnalyzer {
-  type FirstSetTable = Map[Ast.Exp, Set[String]]
-  def calculateFirstSet(grammar: Ast.Grammar, mapping: Map[Ast.Ident, Ast.Exp]): FirstSetTable = {
+import com.github.kmizu.llwan.Ast.Choices
+
+class GrammarAnalyzer(grammar: Ast.Grammar) {
+  type FirstSetTable = Map[Symbol, Set[String]]
+  private[this] val mapping: Map[Symbol, Choices] = grammar.rules.map{ rule => rule.name -> rule.body}.toMap
+
+  def calculateFirstSet: FirstSetTable = {
     def first(e: Ast.Exp, visit: Set[Symbol]): Set[String] = e match {
       case Ast.Choices(_, choices) =>
         def firsts(seq: List[Ast.Prm]): Set[String] = seq match {
@@ -16,13 +17,13 @@ object GrammarAnalyzer {
         }
         choices.foldLeft(Set[String]()){(set, choice) => set ++ firsts(choice)}
       case ident@Ast.Ident(_, name) =>
-        if(visit.contains(name)) Set() else first(mapping(ident), visit + name)
+        if(visit.contains(name)) Set() else first(mapping(name), visit + name)
       case Ast.Str(_, c) =>
         Set(c)
       case Ast.Emp(_) =>
         Set("")
     }
-    (mapping.map { case (ident, exp) => (ident) -> first(mapping(ident), Set(ident.name)) }: FirstSetTable)
+    (mapping.map { case (ident, exp) => (ident) -> first(mapping(ident), Set(ident)) }: FirstSetTable)
   }
   def calculateFollowSet(grammar: Ast.Grammar) = ???
   def calculateDirectorSet(grammar: Ast.Grammar) = ???
